4960ee01eeb3d2799f9f6d5c5464b6b6569f9370
HaoTang0619 <b06902020@ntu.edu.tw>
2022-10-24 07:00:08 +0800

    Usage: add basic subfolder for usage testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 export const HelloWorld = () => 'Hello World!';
+
+export { BSet, NSet, SSet } from './type';
+
+export { default as marshaler } from './marshaler';
+export { default as unmarshaler } from './unmarshaler';
+
+export { default as dataSetToExpressions } from './expressions';
+export { default as logical } from './expressions/logical';
+export * from './expressions/logical';
+export { default as operate } from './expressions/operate';
+export * from './expressions/operate';

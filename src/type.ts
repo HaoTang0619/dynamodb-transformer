57079a256f79c1aed3be7b2f9e80601f727397ed
HaoTang0619 <b06902020@ntu.edu.tw>
2022-10-25 19:48:01 +0800

    Src\Type: rewrite Marshalled to reduce bundle size

--- src/type.ts
+++ src/type.ts
@@ -2,19 +2,6 @@ export class BSet extends Set<Uint8Array> {}
 export class NSet extends Set<number> {}
 export class SSet extends Set<string> {}
 
-type Explode<T> = keyof T extends infer K
-  ? K extends unknown
-    ? { [I in keyof T]: I extends K ? T[I] : never }
-    : never
-  : never;
-
-type AtMostOne<T> = Explode<Partial<T>>;
-
-type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> &
-  U[keyof U];
-
-type ExactlyOne<T> = AtMostOne<T> & AtLeastOne<T>;
-
 export type Unmarshalled =
   | Uint8Array // B
   | boolean // BOOL
@@ -27,15 +14,21 @@ export type Unmarshalled =
   | string // S
   | SSet; // SS
 
-export type Marshalled = ExactlyOne<{
-  B: Uint8Array;
-  BOOL: boolean;
-  BS: Uint8Array[];
-  L: Marshalled[];
-  M: { [name: string]: Marshalled };
-  N: string;
-  NS: string[];
-  NULL: true;
-  S: string;
-  SS: string[];
-}>;
+type UnionKeys<T> = T extends T ? keyof T : never;
+type StrictUnionHelper<T, TAll> = T extends any
+  ? T & Partial<Record<Exclude<UnionKeys<TAll>, keyof T>, never>>
+  : never;
+type StrictUnion<T> = StrictUnionHelper<T, T>;
+
+export type Marshalled = StrictUnion<
+  | { B: Uint8Array }
+  | { BOOL: boolean }
+  | { BS: Uint8Array[] }
+  | { L: Marshalled[] }
+  | { M: { [name: string]: Marshalled } }
+  | { N: string }
+  | { NS: string[] }
+  | { NULL: true }
+  | { S: string }
+  | { SS: string[] }
+>;

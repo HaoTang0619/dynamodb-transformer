1d198ec9f0399d64113d88affb554eee4e81ce1e
HaoTang0619 <b06902020@ntu.edu.tw>
2022-10-26 18:45:08 +0800

    WIP on Src: tune Marshler related types

--- src/marshaler.ts
+++ src/marshaler.ts
@@ -1,63 +1,73 @@
-import { BSet, Marshalled, NSet, SSet, Unmarshalled } from './type';
+import {
+  BSet,
+  MarshalerOfEachResult,
+  MarshalerParams,
+  MarshalerResult,
+  NSet,
+  SSet,
+  Unmarshalled,
+} from './type';
 
-const marshalerOfEach = (value: Unmarshalled): Marshalled => {
+const marshalerOfEach = <T extends Unmarshalled>(
+  value: T,
+): MarshalerOfEachResult<T> => {
   // B
   if (value instanceof Uint8Array) {
-    return { B: value };
+    return { B: value } as MarshalerOfEachResult<T>;
   }
 
   // BOOL
   if (typeof value === 'boolean') {
-    return { BOOL: value };
+    return { BOOL: value } as MarshalerOfEachResult<T>;
   }
 
   // BS
   if (value instanceof BSet) {
-    return { BS: [...value] };
+    return { BS: [...value] } as MarshalerOfEachResult<T>;
   }
 
   // L
   if (Array.isArray(value)) {
-    return { L: value.map((v) => marshalerOfEach(v)) };
+    return {
+      L: value.map((v) => marshalerOfEach(v)),
+    } as MarshalerOfEachResult<T>;
   }
 
   // M is the last
   // N
   if (typeof value === 'number') {
-    return { N: String(value) };
+    return { N: String(value) } as MarshalerOfEachResult<T>;
   }
 
   // NS
   if (value instanceof NSet) {
-    return { NS: [...value].map(String) };
+    return { NS: [...value].map(String) } as MarshalerOfEachResult<T>;
   }
 
   // NULL
   if (value === null) {
-    return { NULL: true };
+    return { NULL: true } as MarshalerOfEachResult<T>;
   }
 
   // S
   if (typeof value === 'string') {
-    return { S: value };
+    return { S: value } as MarshalerOfEachResult<T>;
   }
 
   // SS
   if (value instanceof SSet) {
-    return { SS: [...value] };
+    return { SS: [...value] } as MarshalerOfEachResult<T>;
   }
 
   // M
-  return { M: marshaler(value) };
+  return { M: marshaler(value) } as MarshalerOfEachResult<T>;
 };
 
-const marshaler = (data: {
-  [name: string]: Unmarshalled;
-}): { [name: string]: Marshalled } => {
+const marshaler = <T extends MarshalerParams>(data: T): MarshalerResult<T> => {
   return Object.entries(data).reduce(
     (acc, [name, value]) => ({ ...acc, [name]: marshalerOfEach(value) }),
     {},
-  );
+  ) as MarshalerResult<T>;
 };
 
 export default marshaler;
--- src/type.ts
+++ src/type.ts
@@ -2,11 +2,12 @@ export class BSet extends Set<Uint8Array> {}
 export class NSet extends Set<number> {}
 export class SSet extends Set<string> {}
 
+// For marshaler:
 export type Unmarshalled =
   | Uint8Array // B
   | boolean // BOOL
   | BSet // BS
-  | Unmarshalled[] // L
+  | [Unmarshalled, ...Unmarshalled[]] // L
   | { [name: string]: Unmarshalled } // M
   | number // N
   | NSet // NS
@@ -14,6 +15,52 @@ export type Unmarshalled =
   | string // S
   | SSet; // SS
 
+type MapParamList<T> = T extends Unmarshalled
+  ? MarshalerOfEachResult<T>
+  : never;
+
+type Mapped<
+  Arr extends [...unknown[]],
+  Result extends unknown[] = [],
+> = Arr extends []
+  ? []
+  : Arr extends [infer H]
+  ? [...Result, MapParamList<H>]
+  : Arr extends [infer Head, ...infer Tail]
+  ? Mapped<[...Tail], [...Result, MapParamList<Head>]>
+  : Result;
+
+export type MarshalerOfEachResult<T extends Unmarshalled> = T extends Uint8Array
+  ? { B: Uint8Array }
+  : T extends boolean
+  ? { BOOL: boolean }
+  : T extends BSet
+  ? { BS: Uint8Array[] }
+  : T extends [Unmarshalled, ...Unmarshalled[]]
+  ? { L: Mapped<T> }
+  : T extends { [name: string]: Unmarshalled }
+  ? { M: MarshalerResult<T> }
+  : T extends number
+  ? { N: string }
+  : T extends NSet
+  ? { NS: string[] }
+  : T extends null
+  ? { NULL: true }
+  : T extends string
+  ? { S: string }
+  : T extends SSet
+  ? { SS: string[] }
+  : never;
+
+export type MarshalerParams = {
+  [name: string]: Unmarshalled;
+};
+
+export type MarshalerResult<T extends MarshalerParams> = {
+  [P in keyof T]: MarshalerOfEachResult<T[P]>;
+};
+
+// TODO: For Unmarshaler:
 type UnionKeys<T> = T extends T ? keyof T : never;
 type StrictUnionHelper<T, TAll> = T extends any
   ? T & Partial<Record<Exclude<UnionKeys<TAll>, keyof T>, never>>
